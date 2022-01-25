33970e1efba74bf2cdba4e5f3244185c8ba2b1e3
rjridle <ryan.ridley@okstate.edu>
2022-01-25 14:19:58 -0600

    Added platforms directory to calibration_sky130

---
+++ src/rcx/calibration_sky130/platforms/asap7/setRC.tcl
@@ -0,0 +1,21 @@
+set_layer_rc -layer M1 -capacitance 0.000227856 -resistance 0.002160583
+set_layer_rc -layer M2 -capacitance 0.00022864  -resistance 0.001832146
+set_layer_rc -layer M3 -capacitance 0.000228541 -resistance 0.001832146
+set_layer_rc -layer M4 -capacitance 0.000228674 -resistance 0.001832146
+set_layer_rc -layer M5 -capacitance 0.000228622 -resistance 0.001832146
+set_layer_rc -layer M6 -capacitance 0.000228624 -resistance 0.001832146
+set_layer_rc -layer M7 -capacitance 0.000230714 -resistance 0.001832146
+set_layer_rc -layer M8 -capacitance 0.000362444 -resistance 0.000065401
+set_layer_rc -layer M9 -capacitance 0.000355205 -resistance 0.000065401
+
+set_layer_rc -via V1 -resistance 10E-3
+set_layer_rc -via V2 -resistance 10E-3
+set_layer_rc -via V3 -resistance 10E-3
+set_layer_rc -via V4 -resistance 10E-3
+set_layer_rc -via V5 -resistance 10E-3
+set_layer_rc -via V6 -resistance 10E-3
+set_layer_rc -via V7 -resistance 10E-3
+set_layer_rc -via V8 -resistance 10E-3
+set_layer_rc -via V9 -resistance 10E-3
+
+set_wire_rc -layer M3
